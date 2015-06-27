a53535880544e5c46569ff58c64034c3d18bb1dc
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-06-26 21:05:40 -0500

    feat: initiates Menu module

---
+++ assets/js/MenuModule/controller.js
@@ -0,0 +1,9 @@
+(function(){
+	'use strict';
+
+	angular.module('eveBook.controllers')
+		.controller('menuCtrl', 
+			['$scope',function ($scope) {
+				
+			}]);
+})();
---
+++ assets/js/MenuModule/factory.js
@@ -0,0 +1,12 @@
+(function wrapper() {
+    'use strict';
+
+    angular.module('eveBook.services')
+        .factory('about', ['$http', function aboutFactory ($http) {
+        	
+        	
+            var data = {};
+
+            return data;
+        }]);
+})();
