9a88fa8ea2275dbba17c452762263452b8a07611
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-07-04 19:55:04 -0500

    feat: animations module

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,11 +3,14 @@
 
 	angular.module('eveBook', [
 		'ngRoute',
+		'ngAnimate',
+		'eveBook.animations',
 		'eveBook.controllers',
 		'eveBook.directives',
 		'eveBook.services'
 		]);
 
+	angular.module('eveBook.animations', []);
 	angular.module('eveBook.controllers', []);
 	angular.module('eveBook.directives', []);
 	angular.module('eveBook.services', []);
