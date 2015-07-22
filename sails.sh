42654bd84a43ecf3ab88aefc59ab5a4b2fa64bdf
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-07-22 18:38:02 -0500

    fix: change the if statement

--- sails.sh
+++ sails.sh
@@ -1,10 +1,10 @@
 
-if [ ! -d ".tmp" ]; then
+if [ ! -d "./.tmp" ]; then
   echo 'BOWER INSTALL'
   bower install
 fi
 
-if [ ! -d ".tmp/public/min" ]; then
+if [ ! -d "./.tmp/public/min" ]; then
   echo 'GRUNT PROD'
 	grunt prod 
 fi
