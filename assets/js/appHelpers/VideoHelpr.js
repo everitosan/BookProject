fe6dd12dcb45d3b0c206f091e821ff62f60386ff
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-07-21 20:56:51 -0500

    feat: general Helpers

---
+++ assets/js/appHelpers/VideoHelpr.js
@@ -0,0 +1,11 @@
+(function(){
+	window.playVideo = function(id) {
+		document.getElementById(id).play();
+	};
+
+	window.pauseVideo = function(id) {
+		document.getElementById(id).pause();
+	};
+
+
+})();
