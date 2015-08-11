45ee945bb2ac433bdcf10cfd09bc29e1487b22fc
everardo sanchez <everardo.sanchez.adesis@gmail.com>
2015-08-10 21:43:39 -0500

    fix: mobile views

--- assets/js/appHelpers/VideoHelpr.js
+++ assets/js/appHelpers/VideoHelpr.js
@@ -1,25 +1,28 @@
 (function(){
 	window.playVideo = function(id) {
-		document.getElementById(id).play();
+		var video = document.getElementById(id);
+		if (video)
+			video.play();
 	};
 
 	window.videoGoTo = function(id, time) {
 		var video = document.getElementById(id);
-		var currT = parseInt(video.currentTime);
+		if (video) {
+			var currT = parseInt(video.currentTime);
 
-
-		if (currT != time) {
-			if (currT == 0) {
-				video.currentTime = time;
-			}
-			else {
-				var seek = setInterval(function() {
-					currT++;
-					video.currentTime = currT;
-					if (currT == time) {
-						clearInterval(seek);
-					}
-				}, 50);
+			if (currT != time) {
+				if (currT == 0) {
+					video.currentTime = time;
+				}
+				else {
+					var seek = setInterval(function() {
+						currT++;
+						video.currentTime = currT;
+						if (currT == time) {
+							clearInterval(seek);
+						}
+					}, 50);
+				}
 			}
 		}
 
